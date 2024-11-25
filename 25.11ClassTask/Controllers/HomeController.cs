02a3e3e2fbbbe158e0b9fb9b2390fac452ca2818
lamiya885 <lamiyabh-bp215@code.edu.az>
2024-11-25 19:59:21 +0400

    first commit

---
+++ 25.11ClassTask/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25._11ClassTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Furniture()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }  public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ 25.11ClassTask/Program.cs
@@ -0,0 +1,27 @@
+namespace _25._11ClassTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
